dca95b0177f8fd058fc4004d1499e2e2d46d1871
misheMatcha <mishe.vong@gmail.com>
2020-01-27 22:51:10 -0800

    Add configureStore

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,10 +3,10 @@ import entitiesReducer from './entities_reducer';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 
-const RootReducer = combineReducers({
+const rootReducer = combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
   errors: errorsReducer
 });
 
-export default RootReducer;
+export default rootReducer;
