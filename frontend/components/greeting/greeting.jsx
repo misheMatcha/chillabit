09081fab2b57ec66c895f3b49bb8f6ac38c03f7e
misheMatcha <mishe.vong@gmail.com>
2020-01-28 10:58:29 -0800

    Add greeting container and component. Update App.jsx to include greeting container

---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Greeting = ({currentUser, logout}) => {
+  const loggedGreeting = () => (
+    <hgroup className="header-group">
+      <h2 className="header-name">What's up fellow home-skillet biscuit {currentUser.username}!</h2>
+      <button className="header-button" onClick={logout}>Log Out</button>
+    </hgroup>
+  );
+
+  const sessionLinks = () => (
+    <nav className="login-signup">
+      <Link to='/signup'>Sign Up</Link>
+      <Link to='/login'>Login</Link>
+    </nav>
+  );
+  
+  return currentUser ? loggedGreeting() : sessionLinks();
+};
+
+export default Greeting;
