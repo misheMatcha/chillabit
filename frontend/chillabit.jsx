bcee2360ae6d89f57bb62bfc78f8f10468498ba3
misheMatcha <mishe.vong@gmail.com>
2020-01-28 08:27:47 -0800

    Remove window items used for testing.

--- frontend/chillabit.jsx
+++ frontend/chillabit.jsx
@@ -4,7 +4,8 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById('root')
   const store = configureStore()
+
+  const root = document.getElementById('root')
   ReactDOM.render(<Root store={store}/>, root)
 });
