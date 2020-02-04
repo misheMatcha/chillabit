d575047d77c22bb15bd212c7d338bc5660a2a9a7
misheMatcha <mishe.vong@gmail.com>
2020-02-04 14:15:19 -0800

    Add active storage table

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -1,5 +1,13 @@
 class Api::AlbumsController < ApplicationController
-  
+  def create
+  end
+
+  def update
+  end
+
+  def destory
+  end
+
   private
   def album_params
     params.require(:album).permit(:name, :single, :genre, :artist_id)
--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -1,4 +1,18 @@
 class Api::TracksController < ApplicationController
+  def create
+    @track = Track.new(track_params)
+    if @track.save
+      
+    else
+    end
+  end
+
+  def update
+  end
+
+  def destory
+  end
+
   private
   def track_params
     params.require(:track).permit(:name, :album_id, :artist_id)
