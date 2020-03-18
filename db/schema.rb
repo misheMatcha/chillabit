dda577e1b73026ce6e64e8c4bf74d8389cd3b108
misheMatcha <mishe.vong@gmail.com>
2020-03-18 14:23:56 -0700

    change track schema to set album_id to null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_221404) do
+ActiveRecord::Schema.define(version: 2020_03_18_211643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_221404) do
 
   create_table "tracks", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "album_id", null: false
+    t.integer "album_id"
     t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
