98d837824360c872588749f0fa0d127cee589fad
misheMatcha <mishe.vong@gmail.com>
2020-04-16 09:48:43 -0700

    add onmouseenter/leave for hover on track show mini

--- frontend/components/track/track_show/track_show_mini.js
+++ frontend/components/track/track_show/track_show_mini.js
@@ -5,14 +5,22 @@ class TrackShowMini extends React.Component{
     this.props.fetchTrack(2);
   }
 
+  onHover(e){
+    e.target.style.opacity = '100%';
+  }
+  
+  offHover(e){
+    e.target.style.opacity = '0';
+  }
+
   render(){
     console.log("mini: ", this.props)
     return(
       <div className="single-track-container">
-        <img src="https://chillabit-pro.s3-us-west-1.amazonaws.com/the_day.jpg" className="single-track-cover"/>
-        <div className="single-track-hover">
-          play
+        <div className="single-track-hover" onMouseEnter={this.onHover} onMouseLeave={this.offHover}>
+          <button className="fas fa-play-circle"/>
         </div>
+        <img src="https://chillabit-pro.s3-us-west-1.amazonaws.com/the_day.jpg" className="single-track-cover"/>
         <div className="single-track-details">
           <p className="single-track-name">{this.props.track.name}</p>
           <p className="single-track-artist">{this.props.track.artist}</p>
