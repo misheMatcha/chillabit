8f82d88f00443ecf4e615ecfefcb8fcd3d465e4d
misheMatcha <mishe.vong@gmail.com>
2020-04-21 16:00:22 -0700

    add console log functionality for future methods

--- frontend/components/track/track_list/track_list_item.jsx
+++ frontend/components/track/track_list/track_list_item.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class TrackListItem extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      playing: false
+    }
+
+    this.togglePlay = this.togglePlay.bind(this)
+  }
   onHover(e){
     e.target.style.opacity = '100%';
   }
@@ -9,11 +17,21 @@ class TrackListItem extends React.Component{
     e.target.style.opacity = '0';
   }
 
+  togglePlay(){
+    if(this.state.playing === false){
+      this.setState({playing: true});
+      console.log('Now playing');
+    }else{
+      this.setState({playing: false});
+      console.log('Now paused');
+    }
+  }
+
   render(){
     return(
       <div className="track-list-item">
         <div className="track-list-item-hover" onMouseEnter={this.onHover} onMouseLeave={this.offHover}>
-          <button className="fas fa-play-circle"/>
+          <button className="fas fa-play-circle" onClick={this.togglePlay}/>
         </div>
         <img src="https://chillabit-pro.s3-us-west-1.amazonaws.com/the_day.jpg" className="track-list-item-cover"/>
         <div className="track-list-item-details">
