11882d3a705b27203e253ab1f642f93667ca82ec
misheMatcha <mishe.vong@gmail.com>
2020-04-22 18:17:55 -0700

    refactor sidebar to be own component

--- frontend/components/discover/discover.jsx
+++ frontend/components/discover/discover.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TrackList from '../track/track_list/track_list_contianer';
 import { USERS } from '../placeholder_seeds';
+import Sidebar from '../sidebar/sidebar.jsx';
 
 class Discover extends React.Component {
   constructor(props){
@@ -32,96 +33,8 @@ class Discover extends React.Component {
           <TrackList title={`Stay Home`} desc={`Tunes of isolation and self-care`} />
           <TrackList title={`Chillabit Charts`} desc={`The most played tracks on Chillabit this week`} />
         </div>
-
-
         <div className="sidebar-container">
-          <div className="discover-sidebar-advert">
-            <div className="sidebar-advert-details">
-              <div className="sidebar-advert-gradient">HEY<br/>HEY</div>
-              <div className="sidebar-advert-text">
-                <div className="sidebar-advert-text-bold">
-                  Swap tracks
-                  <br/>
-                  Keep the stats
-                </div>
-                <div className="sidebar-advert-text-normal">
-                  A pro account lets you replace tracks without losing stats
-                </div>
-              </div>
-            </div>
-            <button className="sidebar-advert-button">Get pro</button>
-          </div>
-
-          {/* dry up code later */}
-          <div className="sidebar-section">
-            <div className="sidebar-section-nav">
-              <div className="sidebar-section-nav-title">
-                <i className="fas fa-user-friends"/>
-                <p className="sidebar-section-nav-title-text">Who to follow</p>
-              </div>
-              <div className="sidebar-section-nav-title">
-                <i className="fas fa-redo"/>
-                <p className="sidebar-section-nav-title-text">Refresh</p>
-              </div>
-            </div>
-            <div className="sidebar-section-list">
-              <ul className="sidebar-section-list-ul">
-                {
-                  this.state.followers.map((follower, idx) => {
-                    return(
-                      <li key={idx} className="sidebar-section-list-li">
-                        <div className="sidebar-section-list-follower">
-                          <div className="sidebar-section-list-follower-wrap">
-                            <img src={follower.profileImg} className="sidebar-section-list-photo"/>
-                            <div className="sidebar-section-list-follower-details">
-                              <p className="sidebar-section-list-follower-details-username">{follower.username}</p>
-                              <div className="sidebar-section-list-follower-details-text">
-                                <div className="sidebar-section-list-follower-details-info">
-                                  <i className="fas fa-user-friends"/>
-                                  <p className="sidebar-section-list-follower-details-info-p">{follower.followers}</p>
-                                </div>
-                                <div className="sidebar-section-list-follower-details-info margin">
-                                  <i className="fas fa-tag"/>
-                                  <p className="sidebar-section-list-follower-details-info-p">{follower.numTracks}</p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <button className="sidebar-section-list-follower-button" onClick={this.toggleFollow}><i className="fas fa-user-plus"/> Follow
-                          </button>
-                        </div>
-                      </li>
-                    )
-                  })
-                }
-              </ul>
-            </div>
-          </div>
-
-          <div className="sidebar-section">
-            <div className="sidebar-section-nav">
-              <div className="sidebar-section-nav-title">
-                <i className="fas fa-heart"/>
-                <p className="sidebar-section-nav-title-text">{this.state.likes} likes</p>
-              </div>
-              <div className="sidebar-section-nav-title">
-                <p className="sidebar-section-nav-title-text">View all</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="sidebar-section">
-            <div className="sidebar-section-nav">
-              <div className="sidebar-section-nav-title">
-                <i className="far fa-calendar"/>
-                <p className="sidebar-section-nav-title-text">Listening history</p>
-              </div>
-              <div className="sidebar-section-nav-title">
-                <p className="sidebar-section-nav-title-text">View all</p>
-              </div>
-            </div>
-          </div>
-
+          <Sidebar />
         </div>
       </div>
     )
--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -1,26 +1,107 @@
 import React from 'react';
-import SidebarAd from './sidebar_ad.jsx'
-import SidebarFollow from './sidebar_follow.jsx';
-import SidebarLikes from './sidebar_likes.jsx';
-import SidebarHistory from './sidebar_history.jsx';
+import { USERS } from '../placeholder_seeds';
 
 class Sidebar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      likes: 205,
+      followers: USERS,
+      isFollowing: false
+    }
+  }
+
   render(){
     return (
       <div className="sidebar">
-        <div className="sidebar-box ad">
-          <SidebarAd />
+          <div className="discover-sidebar-advert">
+            <div className="sidebar-advert-details">
+              <div className="sidebar-advert-gradient">HEY<br />HEY</div>
+              <div className="sidebar-advert-text">
+                <div className="sidebar-advert-text-bold">
+                  Swap tracks
+                  <br />
+                  Keep the stats
+                </div>
+                <div className="sidebar-advert-text-normal">
+                  A pro account lets you replace tracks without losing stats
+                </div>
+              </div>
+            </div>
+            <button className="sidebar-advert-button">Get pro</button>
+          </div>
+
+          {/* dry up code later */}
+          <div className="sidebar-section">
+            <div className="sidebar-section-nav">
+              <div className="sidebar-section-nav-title">
+                <i className="fas fa-user-friends" />
+                <p className="sidebar-section-nav-title-text">Who to follow</p>
+              </div>
+              <div className="sidebar-section-nav-title">
+                <i className="fas fa-redo" />
+                <p className="sidebar-section-nav-title-text">Refresh</p>
+              </div>
+            </div>
+            <div className="sidebar-section-list">
+              <ul className="sidebar-section-list-ul">
+                {
+                  this.state.followers.map((follower, idx) => {
+                    return (
+                      <li key={idx} className="sidebar-section-list-li">
+                        <div className="sidebar-section-list-follower">
+                          <div className="sidebar-section-list-follower-wrap">
+                            <img src={follower.profileImg} className="sidebar-section-list-photo" />
+                            <div className="sidebar-section-list-follower-details">
+                              <p className="sidebar-section-list-follower-details-username">{follower.username}</p>
+                              <div className="sidebar-section-list-follower-details-text">
+                                <div className="sidebar-section-list-follower-details-info">
+                                  <i className="fas fa-user-friends" />
+                                  <p className="sidebar-section-list-follower-details-info-p">{follower.followers}</p>
+                                </div>
+                                <div className="sidebar-section-list-follower-details-info margin">
+                                  <i className="fas fa-tag" />
+                                  <p className="sidebar-section-list-follower-details-info-p">{follower.numTracks}</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <button className="sidebar-section-list-follower-button" onClick={this.toggleFollow}><i className="fas fa-user-plus" /> Follow
+                          </button>
+                        </div>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            </div>
+          </div>
+
+          <div className="sidebar-section">
+            <div className="sidebar-section-nav">
+              <div className="sidebar-section-nav-title">
+                <i className="fas fa-heart" />
+                <p className="sidebar-section-nav-title-text">{this.state.likes} likes</p>
+              </div>
+              <div className="sidebar-section-nav-title">
+                <p className="sidebar-section-nav-title-text">View all</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="sidebar-section">
+            <div className="sidebar-section-nav">
+              <div className="sidebar-section-nav-title">
+                <i className="far fa-calendar" />
+                <p className="sidebar-section-nav-title-text">Listening history</p>
+              </div>
+              <div className="sidebar-section-nav-title">
+                <p className="sidebar-section-nav-title-text">View all</p>
+              </div>
+            </div>
+          </div>
+
         </div>
-        <div className="sidebar-box follow-sug">
-          <SidebarFollow />
-        </div>
-        <div className="sidebar-box likes">
-          <SidebarLikes />
-        </div>
-        <div className="sidebar-box history">
-          <SidebarHistory />
-        </div>
-      </div>
     )
   }
 }
