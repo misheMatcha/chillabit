810b55e7f6eb241aa679d77fc8e36b736817c9f6
misheMatcha <mishe.vong@gmail.com>
2020-04-22 18:09:19 -0700

    add sass variables to other stylesheets

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -23,7 +23,6 @@ class App extends React.Component{
         <Modal />
         <Route path="/" component={NavBar}/>
         <Switch>
-          {/* <ProtectdRoute exact path="/" component={Discover}/> */}
           <ProtectdRoute exact path="/" component={Discover}/>
           <ProtectdRoute exact path="/discover" component={Discover}/>
           <ProtectdRoute exact path="/Stream" component={TrackShow}/>
