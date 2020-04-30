f429021413e1a4734437f82d47912dd9f29e9581
misheMatcha <mishe.vong@gmail.com>
2020-04-30 15:29:00 -0700

    change app routing for testing. add new 404 structure

--- frontend/components/error_404_page.jsx
+++ frontend/components/error_404_page.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const Error404Page = () => (
-  <div id="error-404">
-    <h1>Oof, you must be lost.</h1>
-    <div className="error-image"></div>
-    <p>Let's point you back in the right direction.
-    <span><Link to="/">Click here to head back.</Link></span>
-    </p>
+  <div className="error-404-container">
+    <div className="error-404-oops">
+      Oops!
+    </div>
+    <div className="error-404-details">
+      <p className="error-404-code">404 - Page not found</p>
+      <p className="error-404-message">
+        The page you are looking for might have been removed<br/>
+        had its name changed or is temporarily unavailable.
+      </p>
+    </div>
+    <button className="error-404-button">Go to homepage</button>
   </div>
 );
 
