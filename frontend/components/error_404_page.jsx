d1c6eed6e89e9bc9f5447293c40a792307f3a01e
misheMatcha <mishe.vong@gmail.com>
2020-04-30 18:33:54 -0700

    add styling to error details and button

--- frontend/components/error_404_page.jsx
+++ frontend/components/error_404_page.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom'
 
 const Error404Page = () => (
   <div className="error-404-container">
-    <div className="error-404-oops">
-      Oops!
-    </div>
-    <div className="error-404-details">
-      <p className="error-404-code">404 - PAGE NOT FOUND</p>
-      <div className="error-404-message">
-        <p>The page you are looking for might have been removed</p>
-        <p>had its name changed or is temporarily unavailable.</p>
+    <div className="error-404">
+      <div className="error-404-oops">
+        Oops!
       </div>
+      <div className="error-404-details">
+        <p className="error-404-code">404 - PAGE NOT FOUND</p>
+        <div className="error-404-message">
+          <p>The page you are looking for might have been removed</p>
+          <p>had its name changed or is temporarily unavailable.</p>
+        </div>
+      </div>
+      <button className="error-404-button">Go to homepage</button>
     </div>
-    <button className="error-404-button">Go to homepage</button>
   </div>
 );
 
