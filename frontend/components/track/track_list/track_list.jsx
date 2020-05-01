760f0eb39c4155c989d2254c4c7c31151c0705c0
misheMatcha <mishe.vong@gmail.com>
2020-05-01 13:13:15 -0700

    add logic for left/right buttons on track list

--- frontend/components/track/track_list/track_list.jsx
+++ frontend/components/track/track_list/track_list.jsx
@@ -6,44 +6,49 @@ class TrackList extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      stage: 0,
-      carouselIdx: 1
+      stage: 1
     }
     
     this.toggleLeft = this.toggleLeft.bind(this);
     this.toggleRight = this.toggleRight.bind(this);
-    this.showSlides = this.showSlides.bind(this);
   }
   componentDidMount(){
     // this.props.fetchAllTracks();
   }
   
   toggleLeft(){
-    this.state.carouselIdx -= 1;
-    console.log(this.state.carouselIdx)
+    if(this.state.stage > 1){
+      this.state.stage -= 1;
+    }
+    this.toggleDisplay(this.state.stage)
   }
   
   toggleRight(){
-    this.state.carouselIdx += 1;
-    console.log(this.state.carouselIdx)
+    if(this.state.stage < 3){
+      this.state.stage += 1;
+    }
+    this.toggleDisplay(this.state.stage)
   }
+  
+  toggleDisplay(n){
+    let stage1 = document.getElementsByClassName("track-list-li-1");
+    let stage2 = document.getElementsByClassName("track-list-li-2");
+    let stage3 = document.getElementsByClassName("track-list-li-3");
 
-  showSlides(n){
-    let i;
-    const slides = document.getElementsByClassName("track-list-li");
-
-    if(n > TRACKS.length){
-      this.setState({carouselIdx: 1})
+    switch (n) {
+      case 1:
+        stage1.classList.remove('display-none');
+        stage2.classList.add('display-none');
+        stage3.classList.add('display-none');
+      case 2:
+        stage1.classList.add('display-none');
+        stage2.classList.remove('display-none');
+        stage3.classList.add('display-none');
+      case 3:
+        stage1.classList.add('display-none');
+        stage2.classList.add('display-none');
+        stage3.classList.remove('display-none');
     }
-    if(n < 1){
-      this.setState({carouselIdx: TRACKS.length});
-    }
-    
-    for(i = 0; i < TRACKS.length; i++){
-      TRACKS[i].style.display = "none";
-    }
-
-    TRACKS[this.state.carouselIdx-1].style.display = "block";
   }
 
   render(){
@@ -56,13 +61,20 @@ class TrackList extends React.Component{
           <ul className="track-list-ul">
             {
               TRACKS.map((track, idx) => {
-                // if(idx >= this.state.startIdx && (idx <= this.state.endIdx)){
+                let stageIdx;
+                if(idx >= 0 && idx <= 3){
+                  stageIdx = 1;
+                } else if (idx >= 4 && idx <= 7){
+                  stageIdx = 2;
+                } else{
+                  stageIdx = 3;
+                }
+                
                   return(
-                    <li className="track-list-li" key={idx}>
+                    <li className={'track-list-li-' + stageIdx} key={idx}>
                       <TrackListItem track={track} />
                     </li>
                   )
-                // }
               })
             }
           </ul>
