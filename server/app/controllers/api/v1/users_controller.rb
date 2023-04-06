4f9b2b695bf88a90c44398779a13368c61cb8194
Michelle Vong <mishe.vong@gmail.com>
2023-04-05 23:16:56 -0700

    Rebuild #2 - add users controller, view, checked validations

---
+++ server/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render 'api/v1/users/show'
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :age, :gender)
+  end
+end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, :username, :password_digest, :age, :gender, :url, presence: true
   validates :email, :username, :url, uniqueness: true
+  validates :age, numericality: { only_integer: true }
   validates :password, length: { minimum: 8 }
+
+  before_validation :generate_profile_url
+
+  private
+
+  def generate_profile_url
+    self.url = username + '-' + rand(9999).to_s
+  end
 end
