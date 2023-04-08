15f993eb2d1979e970a49a198bc86386494f9193
Michelle Vong <mishe.vong@gmail.com>
2023-04-07 17:37:47 -0700

    Rebuild #3 - add verify_handle route

--- server/app/controllers/api/v1/authenticates_controller.rb
+++ server/app/controllers/api/v1/authenticates_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AuthenticatesController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: %i[create verify_handle]
 
   def create
     @user = User.find_by(email: authenticate_params[:email])
@@ -12,9 +12,34 @@ class Api::V1::AuthenticatesController < ApplicationController
     end
   end
 
+  def verify_handle
+    if authenticate_params[:email] == '' && authenticate_params[:url] == ''
+      return render json: { message: 'Enter a valid email address or profile url.' }, status: :unprocessable_entity
+    end
+
+    email = nil
+    if authenticate_params[:email] != ''
+      email = User.find_by(email: authenticate_params[:email])
+    elsif authenticate_params[:url] != ''
+      email = User.find_by(url: authenticate_params[:url])
+    end
+
+    isVerified = false
+    if email
+      isVerified = true
+      email = email.email
+    elsif authenticate_params[:url] != ''
+      return render json: { message: 'That profile url does not exist' }, status: :unprocessable_entity
+    elsif authenticate_params[:email] != ''
+      email = authenticate_params[:email]
+    end
+
+    render json: { email: email, isVerified: isVerified }, status: :ok
+  end
+
   private
 
   def authenticate_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :url)
   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: 'json' } do
       post 'login', to: 'authenticates#create'
+      post 'verify/handle', to: 'authenticates#verify_handle'
       resources :users
     end
   end
