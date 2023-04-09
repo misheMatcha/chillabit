5a21d69bbf1151e29ab82fbb7ea703202c99219c
Michelle Vong <mishe.vong@gmail.com>
2023-04-09 12:00:37 -0700

    Rebuild #3 - add handle verification to form

--- client/src/components/Authenticate/AuthForm.jsx.js
+++ client/src/components/Authenticate/AuthForm.jsx.js
@@ -3,6 +3,7 @@ import Button from 'antd/lib/button';
 import Form from 'antd/lib/form';
 import Input from 'antd/lib/input';
 import InputNumber from 'antd/lib/input-number';
+import includes from 'lodash/includes';
 import { createUseStyles } from 'react-jss';
 import useAuth from '../../hooks/useAuth';
 import axios from '../../utils/axios';
@@ -22,8 +23,11 @@ const useStyles = createUseStyles({
 
 const AuthForm = () => {
 	const [step, setStep] = useState(1);
+	const [errors, setErrors] = useState({});
 	const { isVerified } = useAuth();
 	const classes = useStyles(step);
+
+	const [form] = Form.useForm();
 
 	const signUpUser = (values) => {
 		let newUser = { user: { ...values } };
@@ -44,9 +48,32 @@ const AuthForm = () => {
 			.catch((err) => console.log(err.response.data));
 	};
 
+	const verifyHandle = () => {
+		setErrors({});
+		const handle = form.getFieldValue('email');
+		let handles = { user: { email: '', url: '' } };
+
+		if (includes(handle, '@')) {
+			handles.user.email = handle;
+		} else {
+			handles.user.url = handle;
+		}
+
+		axios
+			.post('/authenticates/handle', handles)
+			.then((res) => {
+				setStep(2);
+				console.log(res.data);
+			})
+			.catch((err) => {
+				setErrors({ ...err.response.data });
+			});
+	};
+
 	return (
 		<div className={classes.container}>
 			<Form
+				form={form}
 				initialValues={{
 					age: '',
 					email: '',
@@ -62,6 +89,7 @@ const AuthForm = () => {
 					<Form.Item name='email'>
 						<Input placeholder='Your email address or profile URL' />
 					</Form.Item>
+					{errors.message && <div>{errors.message}</div>}
 				</div>
 
 				<div className={classes.step2}>
@@ -96,6 +124,7 @@ const AuthForm = () => {
 				</div>
 
 				<Form.Item>
+					{step === 1 && <Button onClick={verifyHandle}>next step</Button>}
 					<Button htmlType='submit'>Continue</Button>
 				</Form.Item>
 			</Form>
