b4eaa4fc24487efe7f4ff3a1038e31fdd1ddf21e
Michelle Vong <mishe.vong@gmail.com>
2023-04-10 13:23:29 -0700

    Rebuild #4 - add assets and style carousel

--- client/src/pages/Landing/Carousel/index.js
+++ client/src/pages/Landing/Carousel/index.js
@@ -1,57 +1,113 @@
 import React from 'react';
+import { faSoundcloud } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Button from 'antd/lib/button';
+import Carousel from 'antd/lib/carousel';
 import { createUseStyles } from 'react-jss';
 import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 import { CHILLABIT } from '../../../utils/constants';
+import DiscoverBanner from '../assets/discover_banner.jpg';
 
 const useStyles = createUseStyles({
 	container: {
-		backgroundColor: 'orange',
 		borderTop: '4px solid #f50',
 		height: 454,
 		width: 1240,
+		// fontFamily: 'Overpass',
+
+		display: 'flex',
 	},
-	layer1: {},
-	layer2: {},
-	page1: {},
-	page2: {},
+	layer1: {
+		display: 'flex',
+		width: '100%',
+		justifyContent: 'space-between',
+		padding: '13px 30px 0 30px',
+	},
+	layer2: {
+		paddingTop: 145,
+		marginLeft: '-100%',
+		width: '100%',
+	},
+	pageWrapper: {
+		// backgroundColor: 'pink',
+		height: 305,
+		backgroundImage: `url(${DiscoverBanner})`,
+	},
+	page: {
+		// height: 305,
+		// backgroundColor: 'blue',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		height: '100%',
+	},
+	title: {
+		fontFamily: 'Overpass',
+		fontWeight: 500,
+		fontSize: 36,
+		textAlign: 'center',
+		// backgroundColor: 'yellow',
+	},
+	tagline: {
+		fontFamily: 'Overpass',
+		fontWeight: 500,
+		fontSize: 18,
+		width: 530,
+		textAlign: 'center',
+		// backgroundColor: 'purple',
+	},
+	page1Buttons: {
+		// backgroundColor: 'green',
+	},
 });
 
 const LandingCarousel = () => {
 	const classes = useStyles();
 
+	const { toggleModal } = useAuth();
+
 	return (
 		<div className={classes.container}>
 			<div className={classes.layer1}>
-				<div>logo {CHILLABIT}</div>
 				<div>
-					<Button>Sign in</Button>
-					<Button>Create account</Button>
+					<FontAwesomeIcon icon={faSoundcloud} />
+					{CHILLABIT}
+				</div>
+				<div>
+					<Button onClick={() => toggleModal()}>Sign in</Button>
+					<Button onClick={() => toggleModal(true)}>Create account</Button>
 					<Link>For Artists</Link>
 				</div>
 			</div>
 			<div className={classes.layer2}>
-				<div className={classes.page1}>
-					<div>Discover more with {CHILLABIT} Go+</div>
-					<div>
-						{CHILLABIT} Go+ lets you listen offline, ad-free, with over 320 million tracks — and
-						growing.
+				<Carousel>
+					<div className={classes.pageWrapper}>
+						<div className={classes.page}>
+							<div className={classes.title}>Discover more with {CHILLABIT} Go+</div>
+							<div className={classes.tagline}>
+								{CHILLABIT} Go+ lets you listen offline, ad-free, with over 320 million tracks — and
+								growing.
+							</div>
+							<div className={classes.page1Buttons}>
+								<Button>Learn more</Button>
+								<Button>Try it for free for 30 days</Button>
+							</div>
+						</div>
 					</div>
-					<div>
-						<Button>Learn more</Button>
-						<Button>Try it for free for 30 days</Button>
+					<div className={classes.pageWrapper}>
+						<div className={classes.page}>
+							<div className={classes.title}>What's next in music is first on {CHILLABIT}</div>
+							<div className={classes.tagline}>
+								Upload your first track and begin your journey. SoundCloud gives you space to
+								create, find your fans, and connect with other artists.
+							</div>
+							<div>
+								<Button>Start uploading today</Button>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div className={classes.page2}>
-					<div>What's next in music is first on {CHILLABIT}</div>
-					<div>
-						Upload your first track and begin your journey. SoundCloud gives you space to create,
-						find your fans, and connect with other artists.
-					</div>
-					<div>
-						<Button>Start uploading today</Button>
-					</div>
-				</div>
+				</Carousel>
 			</div>
 		</div>
 	);
--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -6,7 +6,16 @@ import useAuth from '../../hooks/useAuth';
 import { CHILLABIT } from '../../utils/constants';
 
 const useStyles = createUseStyles({
-	container: {},
+	container: {
+		backgroundColor: '#fff',
+		width: 1240,
+	},
+	containerWrapper: {
+		backgroundColor: '#f2f2f2',
+		display: 'flex',
+		height: '100vh',
+		justifyContent: 'center',
+	},
 });
 
 const Landing = () => {
@@ -15,47 +24,49 @@ const Landing = () => {
 	const { toggleModal } = useAuth();
 
 	return (
-		<div className={classes.container}>
-			<LandingCarousel />
-			<div>
+		<div className={classes.containerWrapper}>
+			<div className={classes.container}>
+				<LandingCarousel />
 				<div>
-					<div>search bar</div>
-					or
 					<div>
-						<Button>Upload your own</Button>
+						<div>search bar</div>
+						or
+						<div>
+							<Button>Upload your own</Button>
+						</div>
+					</div>
+					<div>Hear what’s trending for free in the {CHILLABIT} community</div>
+					<div>trending and tracks component</div>
+					<div>
+						<Button>Explore trending playlists</Button>
 					</div>
 				</div>
-				<div>Hear what’s trending for free in the {CHILLABIT} community</div>
-				<div>trending and tracks component</div>
 				<div>
-					<Button>Explore trending playlists</Button>
-				</div>
-			</div>
-			<div>
-				<div>Never stop listening</div>
-				<div>{CHILLABIT} is available on Web, iOS, Android, Sonos, Chromecast, and Xbox One.</div>
-			</div>
-			<div>
-				<div>Calling all creators</div>
-				<div>
-					Get on {CHILLABIT} to connect with fans, share your sounds, and grow your audience. What
-					are you waiting for?
+					<div>Never stop listening</div>
+					<div>{CHILLABIT} is available on Web, iOS, Android, Sonos, Chromecast, and Xbox One.</div>
 				</div>
 				<div>
-					<Button>Find out more</Button>
-				</div>
-			</div>
-			<div>
-				<div>Thanks for listening. Now join in.</div>
-				<div>Save tracks, follow artists and build playlists. All for free.</div>
-				<div>
-					<Button onClick={() => toggleModal(true)}>Create account</Button>
+					<div>Calling all creators</div>
 					<div>
-						Already have an account? <Button onClick={() => toggleModal()}>Sign in</Button>
+						Get on {CHILLABIT} to connect with fans, share your sounds, and grow your audience. What
+						are you waiting for?
+					</div>
+					<div>
+						<Button>Find out more</Button>
 					</div>
 				</div>
+				<div>
+					<div>Thanks for listening. Now join in.</div>
+					<div>Save tracks, follow artists and build playlists. All for free.</div>
+					<div>
+						<Button onClick={() => toggleModal(true)}>Create account</Button>
+						<div>
+							Already have an account? <Button onClick={() => toggleModal()}>Sign in</Button>
+						</div>
+					</div>
+				</div>
+				<div>footer</div>
 			</div>
-			<div>footer</div>
 		</div>
 	);
 };
