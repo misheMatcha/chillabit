ddb19432a2f5ca2f5fe484824497e9e4e36ce9be
Michelle Vong <mishe.vong@gmail.com>
2023-05-01 15:17:18 -0700

    Rebulid #8 - update axios baseUrl

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// const devBaseUrl = 'http://localhost:3001/api/v1';
+const proBaseUrl = 'https://chillabit.herokuapp.com/api/v1/';
+
 const instance = axios.create({
-	baseURL: 'http://localhost:3001/api/v1',
+	baseURL: proBaseUrl,
 });
 
 export default instance;
