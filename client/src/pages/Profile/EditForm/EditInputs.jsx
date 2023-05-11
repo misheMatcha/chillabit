2c1d8ad50b554502071310ba14f3d86cd6f1f406
Michelle Vong <mishe.vong@gmail.com>
2023-05-10 23:22:00 -0700

    Enh #7 - add outside click event listener for url

--- client/src/pages/Profile/EditForm/EditInputs.jsx
+++ client/src/pages/Profile/EditForm/EditInputs.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { faPencil } from '@fortawesome/free-solid-svg-icons';
 import Form from 'antd/lib/form';
 import Input from 'antd/lib/input';
 import { createUseStyles, useTheme } from 'react-jss';
 import StyledButton from '../../../components/General/StyledButton';
 import StyledFormItem from '../../../components/General/StyledFormItem';
-import StyledInput from '../../../components/General/StyledInput';
 import { CHILLABIT } from '../../../utils/constants';
 import { styles } from '../../../utils/styles';
 
-const { alignItems, displayFlex, justifyContent, radius, spacing, typography } = styles;
+const { alignItems, displayFlex, justifyContent, radius, spacing, typography, weight } = styles;
 
 const useStyles = createUseStyles((theme) => ({
 	container: {
@@ -22,26 +21,36 @@ const useStyles = createUseStyles((theme) => ({
 		...displayFlex,
 		...justifyContent.spaceBetween,
 	},
-	url: {
-		...alignItems.center,
-		...displayFlex,
-	},
 	urlForm: {
+		'& .ant-form-item-control-input-content': {
+			...alignItems.center,
+			...displayFlex,
+		},
+		'& .ant-form-item-control-input::before': {
+			color: '#666',
+			content: `'${CHILLABIT}.com/'`,
+			marginRight: ({ editUrl }) => (editUrl ? 0 : spacing['0_5']),
+			textTransform: 'lowercase',
+		},
 		margin: `${spacing['0_5']}px 0 ${spacing['1_5']}px`,
 	},
 	urlInput: {
+		'&:focus, &:hover': {
+			borderColor: '#ccc',
+			boxShadow: 'none',
+		},
 		...typography.captions,
+		backgroundColor: theme.color.white,
+		borderColor: '#ccc',
+		borderRadius: radius[4],
+		color: '#333',
 		fontSize: 13,
+		fontWeight: weight[400],
 		height: spacing['3_25'],
 		padding: spacing['0_25'],
 	},
 	urlPath: {
 		flexGrow: 1,
-	},
-	urlPrefix: {
-		color: '#999',
-		marginRight: ({ editUrl }) => (editUrl ? 0 : spacing['0_5']),
-		textTransform: 'lowercase',
 	},
 }));
 
@@ -50,6 +59,18 @@ const EditInputs = () => {
 	const [editUrl, setEditUrl] = useState(false);
 	const classes = useStyles({ editUrl, theme });
 	const form = Form.useFormInstance();
+	const ref = useRef(null);
+
+	useEffect(() => {
+		const handleOutsideClick = (e) => {
+			if (ref.current && !ref.current.input.contains(e.target)) setEditUrl(false);
+		};
+
+		document.addEventListener('click', handleOutsideClick, true);
+		return () => {
+			document.removeEventListener('click', handleOutsideClick, true);
+		};
+	}, [ref]);
 
 	return (
 		<div className={classes.container}>
@@ -64,25 +85,21 @@ const EditInputs = () => {
 				name='url'
 				required
 			>
-				<div className={classes.url}>
-					<span className={classes.urlPrefix}>{`${CHILLABIT}.com/`}</span>
-					{editUrl ? (
-						<div className={classes.urlPath}>
-							<StyledInput
-								name='url'
-								styles={classes.urlInput}
-							/>
-						</div>
-					) : (
-						<>
-							<span className={classes.urlPath}>{form.getFieldValue('url')}</span>
-							<StyledButton
-								icon={faPencil}
-								onClick={() => setEditUrl(true)}
-							/>
-						</>
-					)}
-				</div>
+				{editUrl ? (
+					<Input
+						name='url'
+						ref={ref}
+						className={classes.urlInput}
+					/>
+				) : (
+					<>
+						<span className={classes.urlPath}>{form.getFieldValue('url')}</span>
+						<StyledButton
+							icon={faPencil}
+							onClick={() => setEditUrl(true)}
+						/>
+					</>
+				)}
 			</StyledFormItem>
 			<div className={classes.row}>
 				<StyledFormItem
