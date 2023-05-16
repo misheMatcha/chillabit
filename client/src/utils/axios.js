85cb0526cd6c81f9aa086724eb45a60139f304bd
Michelle Vong <mishe.vong@gmail.com>
2023-05-15 21:34:37 -0700

    Fix #1 - fix axios url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001/api/v1';
-// const baseURL = 'https://chillabit.herokuapp.com/api/v1/';
+// const baseURL = 'http://localhost:3001/api/v1';
+const baseURL = 'https://chillabit.herokuapp.com/api/v1/';
 
 const instance = axios.create({
 	baseURL: baseURL,
