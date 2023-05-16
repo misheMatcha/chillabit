527d1e8511f14645bac1df00a95ac7271840ed97
Michelle Vong <mishe.vong@gmail.com>
2023-05-15 19:54:14 -0700

    Enh #5 - add description and truncation

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { faEarthAmerica } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { faCaretDown, faCaretUp, faEarthAmerica } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as cn from 'classnames';
 import { createUseStyles, useTheme } from 'react-jss';
 import { Link } from 'react-router-dom';
+import { LINKS_PLACEHOLDER } from '../data/userPlaceholders';
 import useGeneral from '../hooks/useGeneral';
 import { styles } from '../utils/styles';
 
@@ -21,15 +23,36 @@ const {
 
 const useStyles = createUseStyles((theme) => ({
 	container: {
+		'& a': {
+			textDecoration: theme.link.textDecoration.standard,
+		},
 		paddingLeft: 30,
 		width: 360,
+	},
+	description: {
+		marginBottom: 10,
+	},
+	descriptionShow: {
+		'& > :last-child': {
+			marginLeft: spacing[1],
+		},
+		'& > svg': {
+			fontSize: 16,
+		},
+		...alignItems.center,
+		...displayFlex,
+		...typography.captions,
+		color: theme.color.black,
+		fontWeight: weight[400],
+	},
+	descriptionWrapper: {
+		marginBottom: spacing['2_5'],
 	},
 	link: {
 		'&:hover': {
 			color: '#333',
 		},
 		color: '#999',
-		textDecoration: theme.link.textDecoration.standard,
 	},
 	linkIcon: {
 		marginRight: spacing[1],
@@ -55,7 +78,6 @@ const useStyles = createUseStyles((theme) => ({
 		fontWeight: weight[500],
 		paddingBottom: 1,
 		paddingTop: 1,
-		textDecoration: theme.link.textDecoration.standard,
 	},
 	statsTitle: {
 		...typography.captions,
@@ -64,54 +86,86 @@ const useStyles = createUseStyles((theme) => ({
 	},
 	statsWrapper: {
 		...displayFlex,
+		marginBottom: spacing['1_7'],
+	},
+	truncated: {
+		maxHeight: 85,
+		overflow: 'hidden',
+	},
+	truncatedWrapper: {
+		'&::after': {
+			background: 'linear-gradient(hsla(0,0%,100%,0),#fff)',
+			bottom: 54,
+			content: "''",
+			display: 'block',
+			height: 25,
+			position: 'relative',
+		},
 	},
 }));
 
-const LINKS_PLACEHOLDER = [
-	{
-		title: 'Tik Tok',
-		url: '',
-	},
-	{
-		title: 'pachacco',
-		url: '',
-	},
-	{
-		title: 'badtz',
-		url: '',
-	},
-	{
-		title: 'star',
-		url: '',
-	},
-	{
-		title: 'orange',
-		url: '',
-	},
-];
+// refac when working on main sidebar
 
 const Sidebar = () => {
 	const theme = useTheme();
 	const classes = useStyles({ theme });
 	const { user } = useGeneral();
+	const [showMore, setShowMore] = useState(false);
+
+	const toggleShowMore = () => setShowMore(!showMore);
 
 	return (
 		<div className={classes.container}>
 			<div className={classes.statsWrapper}>
-				<Link className={classes.stats}>
+				<Link
+					className={classes.stats}
+					to={`/${user.url}/followers`}
+				>
 					<div className={classes.statsTitle}>Followers</div>
 					<div>277K</div>
 				</Link>
-				<Link className={classes.stats}>
+				<Link
+					className={classes.stats}
+					to={`/${user.url}/following`}
+				>
 					<div className={classes.statsTitle}>Following</div>
 					<div>5</div>
 				</Link>
-				<Link className={classes.stats}>
+				<Link
+					className={classes.stats}
+					to={`/${user.url}/tracks`}
+				>
 					<div className={classes.statsTitle}>Tracks</div>
 					<div>143</div>
 				</Link>
 			</div>
-			<div>bio</div>
+			{/* {user.bio && ( */}
+			<div
+				className={cn(classes.descriptionWrapper, { [`${classes.truncatedWrapper}`]: !showMore })}
+			>
+				<div className={cn(classes.description, { [`${classes.truncated}`]: !showMore })}>
+					{user.bio}
+					im a place holder :)
+				</div>
+				{showMore ? (
+					<Link
+						className={classes.descriptionShow}
+						onClick={toggleShowMore}
+						to='#'
+					>
+						Show less <FontAwesomeIcon icon={faCaretUp} />
+					</Link>
+				) : (
+					<Link
+						className={classes.descriptionShow}
+						onClick={toggleShowMore}
+						to='#'
+					>
+						Show more <FontAwesomeIcon icon={faCaretDown} />
+					</Link>
+				)}
+			</div>
+			{/* )} */}
 			{user.links && (
 				<ul className={classes.links}>
 					{LINKS_PLACEHOLDER.map((link, i) => (
---
+++ client/src/data/userPlaceholders.js
@@ -0,0 +1,22 @@
+export const LINKS_PLACEHOLDER = [
+	{
+		title: 'Tik Tok',
+		url: '',
+	},
+	{
+		title: 'pachacco',
+		url: '',
+	},
+	{
+		title: 'badtz',
+		url: '',
+	},
+	{
+		title: 'star',
+		url: '',
+	},
+	{
+		title: 'orange',
+		url: '',
+	},
+];
