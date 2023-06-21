1e337c38067ad19d4f9986d3a1684d8c354eac0f
Michelle Vong <mishe.vong@gmail.com>
2023-06-20 20:33:45 -0700

    Feat #5 - style and update Metadata

--- client/src/pages/Upload/UploadForm/UploadMetadata.jsx
+++ client/src/pages/Upload/UploadForm/UploadMetadata.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { faSquareCheck } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as cn from 'classnames';
 import { createUseStyles, useTheme } from 'react-jss';
-import StyledFormItem from '../../../components/General/StyledFormItem';
+import FormItem from '../../../components/Form/FormItem';
+import { Y_N_Options } from '../../../data/trackPlaceholders';
 import { styles } from '../../../utils/styles';
 
 const { displayFlex, justifyContent, spacing, typography, weight } = styles;
 
 const useStyles = createUseStyles((theme) => ({
-	container: {},
+	container: {
+		'& > div:first-child': {
+			marginBottom: 38,
+			padding: '10px 0',
+		},
+	},
+	input: {
+		padding: '0 5px',
+		width: 250,
+	},
+	item: {
+		flexGrow: 1,
+		padding: '0 5px',
+	},
 	section: {
 		...displayFlex,
+		...justifyContent.spaceBetween,
+		flexWrap: 'wrap',
 	},
 }));
 
@@ -17,50 +36,150 @@ const UploadMetadata = () => {
 	const classes = useStyles({ theme });
 
 	return (
-		<div>
+		<div className={classes.container}>
 			<div className={classes.section}>
-				<StyledFormItem type='checkbox' />
-				<StyledFormItem
+				<FormItem
 					label='Contains music'
-					small
+					name='music'
+					inputConfig={{
+						options: Y_N_Options,
+						type: 'select',
+					}}
+					styles={classes.input}
 				/>
-				<StyledFormItem
+				<FormItem
 					label='Artist'
-					small
+					name='artist_name'
+					inputConfig={{
+						type: 'text',
+					}}
+					styles={classes.item}
 				/>
-				<StyledFormItem
+				<FormItem
 					label='Publisher'
-					small
+					name='publisher'
+					inputConfig={{
+						type: 'text',
+					}}
+					styles={classes.item}
 				/>
-				<StyledFormItem
+				<FormItem
 					label='ISRC'
-					small
+					name='isrc'
+					inputConfig={{
+						placeholder: 'e.g. USS1Z1001234',
+						type: 'text',
+					}}
+					styles={classes.item}
 				/>
-				<StyledFormItem
+				<FormItem
 					label='Composer'
-					small
+					name='composer'
+					inputConfig={{
+						type: 'text',
+					}}
+					styles={classes.item}
 				/>
-				<StyledFormItem
+				<FormItem
 					label='Release title'
-					small
+					name='release_title'
+					inputConfig={{
+						type: 'text',
+					}}
+					styles={classes.item}
 				/>
 			</div>
-			<StyledFormItem label='Buy-link' />
+			<FormItem
+				fullWidth
+				label='Buy-link'
+				name='buy_link'
+				inputConfig={{
+					type: 'text',
+				}}
+				styles={classes.item}
+			/>
 			<div className={classes.section}>
-				<StyledFormItem label='Album title' />
-				<StyledFormItem label='Record label' />
-				<StyledFormItem label='Release date' />
+				<FormItem
+					label='Album title'
+					name='album_title'
+					inputConfig={{
+						type: 'text',
+					}}
+					styles={classes.item}
+				/>
+				<FormItem
+					label='Record label'
+					name='record_label'
+					inputConfig={{
+						type: 'text',
+					}}
+					styles={classes.item}
+				/>
+				<FormItem
+					label='Release date'
+					name='release_date'
+					inputConfig={{
+						type: 'text',
+					}}
+					styles={classes.item}
+				/>
 			</div>
 			<div className={classes.section}>
-				<StyledFormItem label='Barcode' />
-				<StyledFormItem label='ISWC' />
+				<FormItem
+					label='Album Barcode'
+					name='album_barcode'
+					inputConfig={{
+						type: 'text',
+					}}
+					styles={classes.item}
+				/>
+				<FormItem
+					label='ISWC'
+					name='iswc'
+					inputConfig={{
+						placeholder: 'e.g. T-034.524.680-1',
+						type: 'text',
+					}}
+					styles={classes.input}
+				/>
 			</div>
 			<div className={classes.section}>
-				<StyledFormItem label='P line' />
-				<StyledFormItem label='Contains explicit content' />
+				<FormItem
+					label='P line'
+					name='p_line'
+					inputConfig={{
+						placeholder: 'e.g. 2007 XYZ Record Company Limited',
+						type: 'text',
+					}}
+					styles={classes.item}
+				/>
+				<FormItem
+					label='Contains explicit content'
+					name='explicit'
+					inputConfig={{
+						options: Y_N_Options,
+						type: 'select',
+					}}
+					styles={classes.input}
+				/>
 			</div>
-			<div>license</div>
-			<div>radio</div>
+			<div>
+				<div>
+					<FontAwesomeIcon icon={faSquareCheck} /> License
+					<div>
+						<div>radio</div>
+						<div>icon</div>
+					</div>
+					<div>
+						<FormItem
+							inputConfig={{
+								type: 'checkbox',
+								title: 'Attribution',
+							}}
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
