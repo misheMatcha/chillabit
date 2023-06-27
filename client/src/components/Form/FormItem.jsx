9ec36d34f81eca442f8707fdec6240884d052492
Michelle Vong <mishe.vong@gmail.com>
2023-06-26 17:10:31 -0700

    Enh #18 - refactor FormItem

--- client/src/components/Form/FormItem.jsx
+++ client/src/components/Form/FormItem.jsx
@@ -2,98 +2,104 @@ import React from 'react';
 import Form from 'antd/lib/form';
 import * as cn from 'classnames';
 import { createUseStyles, useTheme } from 'react-jss';
-import FormCheckbox from './FormCheckbox';
-import FormInput from './FormInput';
-import FormSelect from './FormSelect';
-import FormTextarea from './FormTextarea';
 import { styles } from '../../utils/styles';
 
 const { alignItems, displayFlex, flexDirection, spacing, typography, weight, width } = styles;
 
 const useStyles = createUseStyles((theme) => ({
 	container: {
-		'& .ant-col': {
-			flex: 0,
+		'& .ant-form-item-control-input': {
+			minHeight: spacing['3_25'],
 		},
 		'& .ant-form-item-explain': {
-			margin: `${spacing['0_7']}px 0`,
+			height: 27.8,
 		},
 		'& .ant-form-item-explain-error': {
 			...typography.captions,
 			color: theme.color.error,
 			fontSize: 13,
+			fontWeight: weight[600],
+			padding: `${spacing['0_7']}px 0 5px`,
 		},
-
 		'& .ant-form-item-label': {
 			'& label': {
+				...displayFlex,
+				...alignItems.flexStart,
+				...typography.captions,
 				fontSize: 13,
-				height: spacing['1_5'],
+				height: 24.8,
 			},
-			...alignItems.flexStart,
-			...displayFlex,
-			fontWeight: weight[500],
-			marginBottom: spacing[1],
 		},
-
 		'& .ant-form-item-required': {
-			'&::after': {
-				color: `${theme.color.error} !important`,
-				content: "'*' !important",
-				fontSize: spacing['1_5'],
-			},
 			'&::before': {
 				display: 'none !important',
 			},
 		},
 		'& .ant-row': {
-			...flexDirection.column,
+			flexGrow: 1,
 		},
+		...displayFlex,
 		marginBottom: 10,
 	},
 	fullWidth: {
 		...width[100].percentage,
+	},
+	marginTop: {
+		marginTop: 10,
+	},
+	required: {
+		...typography.micro,
+		color: theme.color.error,
+		marginLeft: spacing['0_25'],
+	},
+	topLabel: {
+		'& .ant-row': {
+			...flexDirection.column,
+		},
 	},
 }));
 
 const FormItem = ({
 	children,
 	label,
-	name,
-	rules,
 	styles,
-	valuePropName,
-	fullWidth = false,
-	inputConfig = {},
+	colon = false,
+	marginTop = true,
+	topLabel = true,
+	...props
 }) => {
 	const theme = useTheme();
 	const classes = useStyles({ theme });
 
-	const getInput = (type) => {
-		switch (type) {
-			case 'text':
-				return <FormInput {...inputConfig} />;
-			case 'select':
-				return <FormSelect {...inputConfig} />;
-			case 'tetextarea':
-				return <FormTextarea {...inputConfig} />;
-			case 'checkbox':
-				return <FormCheckbox {...inputConfig} />;
-			default:
-		}
-	};
-
 	return (
 		<Form.Item
-			className={cn(classes.container, { [`${classes.fullWidth}`]: fullWidth }, styles)}
-			colon={false}
-			label={label}
+			className={cn(
+				classes.container,
+				{
+					[`${classes.marginTop}`]: marginTop,
+					[`${classes.topLabel}`]: topLabel,
+				},
+				styles
+			)}
+			colon={colon}
+			label={
+				props.required ? (
+					<>
+						{label}
+						<span className={classes.required}>*</span>
+					</>
+				) : (
+					label
+				)
+			}
 			labelAlign='left'
-			name={name}
 			validateFirst={true}
-			rules={rules}
-			valuePropName={valuePropName}
+			wrapperCol={{
+				flex: 1,
+			}}
+			{...props}
 		>
-			{children ? children : getInput(inputConfig.type)}
+			{children}
 		</Form.Item>
 	);
 };
