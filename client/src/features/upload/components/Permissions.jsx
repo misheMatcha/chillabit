c0f25fe855acbad6591f283c069e8714ded6cf8c
Michelle Vong <mishe.vong@gmail.com>
2023-06-27 22:15:41 -0700

    Enh #18 - fix Permission styling

--- client/src/features/upload/components/Permissions.jsx
+++ client/src/features/upload/components/Permissions.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { faBan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { createUseStyles, useTheme } from 'react-jss';
-import FormItem from '../../../components/form/FormItem';
+import { FormCheckbox } from '../../../components/form';
 import StyledLink from '../../../components/General/StyledLink';
 import { Step } from '../../../components/steps/index';
 import { CHILLABIT } from '../../../utils/constants';
@@ -14,18 +14,18 @@ const { alignItems, displayFlex, flexDirection, radius, spacing, textAlign, typo
 const useStyles = createUseStyles((theme) => ({
 	access: {
 		'& > svg': {
-			marginRight: 4,
+			marginRight: spacing['0_5'],
 		},
 		...typography.h4,
 		borderBottom: `1px solid ${theme.background.highlight}`,
 		color: '#999',
-		fontSize: 14,
+		fontSize: spacing['1_7'],
 		fontWeight: weight[600],
 		letterSpacing: 0.025,
 	},
 	checkbox: {
 		marginBottom: 0,
-		paddingRight: 14,
+		paddingRight: spacing['1_7'],
 		paddingTop: 10,
 	},
 	checkboxContianer: {
@@ -47,15 +47,15 @@ const useStyles = createUseStyles((theme) => ({
 		...textAlign.center,
 		backgroundColor: theme.background.highlight,
 		border: `1px solid #e5e5e5`,
-		marginTop: 20,
-		padding: `30px 80px`,
+		marginBottom: 25,
+		marginTop: spacing['2_5'],
+		padding: `${spacing['3_7']}px 80px`,
 	},
 	controlInfo: {
 		color: '#999',
-		fontSize: 14,
-		fontWeight: weight[600],
+		fontSize: spacing['1_7'],
+		fontWeight: weight[500],
 		lineHeight: 1.4,
-		margin: `10px 0 20px`,
 	},
 	controlTitle: {
 		...typography.h4,
@@ -66,7 +66,7 @@ const useStyles = createUseStyles((theme) => ({
 		borderColor: '#e5e5e5',
 		borderRadius: radius[3],
 		color: theme.color.black,
-		fontSize: 14,
+		fontSize: spacing['1_7'],
 		padding: `${spacing['0_25']}px 14px`,
 	},
 }));
@@ -77,58 +77,43 @@ const Permissions = () => {
 
 	const checkboxOptions = [
 		{
-			inputConfig: {
-				checkedLabel: (
-					<>
-						This track will be available for direct download in the original format it was uploaded.
-						<div className={classes.checkboxWarning}>
-							Distributing content without permission is unlawful. Make sure you have all necessary
-							rights.
-						</div>
-					</>
-				),
-				label:
-					'This track will not be available for direct download in the original format it was uploaded.',
-				title: 'Enable direct downloads',
-				type: 'checkbox',
-			},
+			checkedLabel: (
+				<>
+					This track will be available for direct download in the original format it was uploaded.
+					<div className={classes.checkboxWarning}>
+						Distributing content without permission is unlawful. Make sure you have all necessary
+						rights.
+					</div>
+				</>
+			),
+			label:
+				'This track will not be available for direct download in the original format it was uploaded.',
 			name: 'download',
+			title: 'Enable direct downloads',
 		},
 		{
-			inputConfig: {
-				checkedLabel: 'This track can be played on devices without an internet connection.',
-				label: 'Playing this track will not be possible on devices without an internet connection.',
-				title: 'Offline listening',
-				type: 'checkbox',
-			},
+			checkedLabel: 'This track can be played on devices without an internet connection.',
+			label: 'Playing this track will not be possible on devices without an internet connection.',
 			name: 'offline',
+			title: 'Offline listening',
 		},
 		{
-			inputConfig: {
-				checkedLabel: 'This track will be included in your RSS feed if it is public.',
-				label: 'This track will not be included in your RSS feed.',
-				title: 'Include in RSS feed',
-				type: 'checkbox',
-			},
+			checkedLabel: 'This track will be included in your RSS feed if it is public.',
+			label: 'This track will not be included in your RSS feed.',
 			name: 'rss',
+			title: 'Include in RSS feed',
 		},
 		{
-			inputConfig: {
-				checkedLabel: 'This track’s embedded-player code will be displayed publicly.',
-				label: 'This track’s embedded-player code will only be displayed to you.',
-				title: 'Display embed code',
-				type: 'checkbox',
-			},
+			checkedLabel: `This track's embedded-player code will be displayed publicly.`,
+			label: `This track's embedded-player code will only be displayed to you.`,
 			name: 'embed',
+			title: 'Display embed code',
 		},
 		{
-			inputConfig: {
-				checkedLabel: `This track will be playable outside of ${CHILLABIT} and its apps.`,
-				label: `This track will not be playable outside of ${CHILLABIT} and its apps.`,
-				title: 'Enable app playback',
-				type: 'checkbox',
-			},
+			checkedLabel: `This track will be playable outside of ${CHILLABIT} and its apps.`,
+			label: `This track will not be playable outside of ${CHILLABIT} and its apps.`,
 			name: 'playback',
+			title: 'Enable app playback',
 		},
 	];
 
@@ -140,12 +125,14 @@ const Permissions = () => {
 						<FontAwesomeIcon icon={faBan} /> Access
 					</div>
 					<div className={classes.checkboxWrapper}>
-						{checkboxOptions.map((option, i) => (
-							<FormItem
-								key={i}
-								name={option.name}
-								inputConfig={option.inputConfig}
+						{checkboxOptions.map(({ formConfig, title, ...option }, i) => (
+							<FormCheckbox
+								special
+								key={`${i}-${title}`}
+								formConfig={formConfig}
 								styles={classes.checkbox}
+								title={title}
+								{...option}
 							/>
 						))}
 					</div>
@@ -153,7 +140,7 @@ const Permissions = () => {
 				<div className={classes.control}>
 					<h2 className={classes.controlTitle}>Be in control</h2>
 					<p className={classes.controlInfo}>
-						With a Next Pro plan, you’re in charge with quiet mode. Choose whether comments should
+						With a Next Pro plan, you're in charge with quiet mode. Choose whether comments should
 						be public, private, or not allowed. You can also show or hide stats.
 					</p>
 					<StyledLink
