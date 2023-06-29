5615509e4b84fb54c07674fd9c308a80582f1f62
Michelle Vong <mishe.vong@gmail.com>
2023-06-28 17:07:12 -0700

    Feat #5 - rename FormDataLayout to UploadDataLayout

--- client/src/features/upload/index.js
+++ client/src/features/upload/index.js
@@ -1,4 +1,4 @@
 export { default as Navbar } from './components/Navbar';
 export { default as Footer } from './components/Footer';
 export { default as FormFiles } from './components/FormFiles';
-export { default as UploadDataLayout } from './components/FormDataLayout';
+export { default as UploadDataLayout } from './components/UploadDataLayout';
