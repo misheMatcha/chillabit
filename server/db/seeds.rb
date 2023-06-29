5e1d823645ec08816e498efc0798e68a5adfe349
Michelle Vong <mishe.vong@gmail.com>
2023-06-29 16:14:49 -0700

    Feat #5 - add testing seed

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+User.create!([
+               {
+                 # full seed later
+                 # for quick testing - remove on prod
+                 email: 'm@m.com',
+                 username: 'm',
+                 password: '1234567889',
+                 age: 99,
+                 gender: 'na',
+                 url: 'm'
+               }
+             ])
