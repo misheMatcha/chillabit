7eff70a99075fb6716eb102d42500562dc66a247
Michelle Vong <mishe.vong@gmail.com>
2023-08-06 17:42:38 -0700

    Enh #18 - add error styles to FormItem

--- client/src/components/form/FormItem.jsx
+++ client/src/components/form/FormItem.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Form from 'antd/lib/form';
 import * as cn from 'classnames';
 import { createUseStyles, useTheme } from 'react-jss';
 import { styles } from '../../utils/styles';
 
-const { alignItems, displayFlex, flexDirection, spacing, typography } = styles;
+const { alignItems, displayFlex, flexDirection, spacing, typography, weight } = styles;
 
 const useStyles = createUseStyles((theme) => ({
-	container: {},
+	container: {
+		'& .ant-form-item-explain-connected': {
+			marginBottom: spacing[1],
+		},
+		'& .ant-form-item-explain-error': {
+			color: theme.color.error,
+			fontSize: 13,
+			fontWeight: weight[600],
+			padding: `6px 0`,
+		},
+		marginBottom: spacing[2],
+	},
 }));
 
 const FormItem = ({ children, styles, colon = false, ...props }) => {
 	const theme = useTheme();
 	const classes = useStyles({ theme });
+
+	useEffect(() => {});
 
 	return (
 		<Form.Item
